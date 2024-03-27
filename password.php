0971a2e2f607676aa5951c102232c4b567e0c4a8
unknown <beka.imnadze@gmail.com>
2024-03-27 13:35:19 +0400

    homework for logic-operators

---
+++ password.php
@@ -0,0 +1,27 @@
+<?php
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = $_POST["username"];
+//     $password = $_POST["password"];
+//     $password_confirmation = $_POST["password_confirmation"];
+
+//     if ($password === $password_confirmation) {
+//         echo ($name);
+//         echo "<br/>";
+//         echo "<strong> Your registration complete. </strong>";
+//     } else {
+//         echo "password and password confirmation <strong> does not match !!! </strong>";
+//     }
+// }
+
+$name = "beka";
+
+$password = $_POST['password'];
+$password_confirmation = $_POST['password_confirmation'];
+
+// echo ($password === $password_confirmation) ? "your Password is correct" : "your Password Wrong!!" . $name; 
+if($password === $password_confirmation) {
+    echo "your Password is correct" . $name;
+} else {
+    echo "your Password Wrong!!" . $name;
+}
